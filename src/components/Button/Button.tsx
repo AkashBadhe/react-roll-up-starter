f8c443e6d8996728488e8772646f7c6c5af36472
Akash Badhe <akashb@arista.com>
2024-04-03 08:55:13 +0530

    feat(lib) update name - Updated lib name - Added button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,21 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Button.scss";
-import styled from 'styled-components';
 
 export interface ButtonProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>{
   label: string;
 }
 
-const Button = styled.button`
-  background: green;
-  color: white;
-  border-radius: 3px;
-  border: none;
-  padding: 10px 20px;
-`;
-
 const CustomButton = ({label, ...others}: ButtonProps) => {
-  return <Button {...others}>{label}</Button>;
+  const [count, setCount] = useState(2);
+  const clickHandler = () => {
+    setCount(count + 1);
+  };
+
+  return <button {...others} onClick={clickHandler}>{label} {count}</button>;
 };
 
 export default CustomButton;
